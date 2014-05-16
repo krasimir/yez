30c50d772d16c661f7ad38cc13c767c367f773e5
Krasimir Tsonev <krasimir@outset.ws>
2014-05-17 00:00:51 +0300

    Fixing the devtools policy issue

--- chrome/js/Yez.js
+++ chrome/js/Yez.js
@@ -1,17 +1,30 @@
 absurd.component('Yez', {
+	host: 'localhost',
 	port: 9172,
-	ready: function() {
-
-		var status = Status();
-
-		var socket = io.connect('http://localhost:' + this.port, {
+	connected: false,
+	connect: function() {
+		if(this.connected) { return; }
+		var self = this;
+		this.socket = io.connect('http://' + this.host + ':' + this.port, {
 			'force new connection': true
 		});
-		socket.on('connect', function () {
-			status.setStatus(true);
+		this.socket.on('connect', function () {
+			self.connected = true;
+			self.status.setStatus(true);
 		});
-		socket.on('disconnect', function() {
-			status.setStatus(false);
+		this.socket.on('disconnect', function() {
+			self.connected = false;
+			self.status.setStatus(false);
+			self.connect();
 		});
+		setTimeout(function() {
+			if(!self.connected) {
+				self.connect();
+			}
+		}, 5000);
+	},
+	ready: function() {
+		this.status = Status(this.host, this.port);
+		this.connect();
 	}
 })();
--- chrome/js/components/Status.js
+++ chrome/js/components/Status.js
@@ -1,25 +1,37 @@
 var Status = absurd.component('Status', {
 	css: {
 		'[data-component="status"]': {
+			pos: 'r',
 			pad: '12px 14px 0 0',
 			fz: '14px',
 			fl: 'r',
 			color: '#9F9F9F',
-			'.fa-flash-connected': {
-				color: '#26A430'
-			},
-			'.fa-flash-disconnected': {
+			'.fa-flash': {
 				color: '#CC151A'
+			},
+			'.desc': {
+				pos: 'a',
+				fz: '12px',
+				right: '14px',
+				top: '60px',
+				wid: '300px',
+				lh: '20px',
+				ta: 'r',
+				d: 'b'
 			}
 		}
 	},
 	html: '[data-component="status"]',
 	status: 'disconnected',
-	constructor: function() {
+	constructor: function(host, port) {
+		this.host = host;
+		this.port = port;
 		this.populate();
 	},
 	setStatus: function(s) {
 		this.status = s ? 'connected' : 'disconnected';
+		this.css['[data-component="status"]']['.desc'].d = s ? 'n' : 'b';
+		this.css['[data-component="status"]']['.fa-flash'].color = s ? '#26A430' : '#CC151A';
 		this.populate();
 	}
 })
